cc92c6e0a6c858257cc75527e989466e38360a92
thisisaaronland <thisisaaronland@localhost>
2023-05-19 12:05:30 -0700

    Bug fix: default ttl in debug crumb should be 300 (seconds or 5 minutes) not 5 (seconds)

--- debug.go
+++ debug.go
@@ -3,6 +3,8 @@ package crumb
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strconv"
 )
 
 func init() {
@@ -13,11 +15,35 @@ func init() {
 // NewDebugCrumb returns a `EncryptedCrumb` instance with a randomly generated secret and salt valid
 // for 5 minutes configured by 'uri' which should take the form of:
 //
-//	debug://
+//	debug://?{QUERY_PARAMETERS}
+//
+// Where '{QUERY_PARAMETERS}' may be:
+// * `ttl={SECONDS}`. Default is 300
 func NewDebugCrumb(ctx context.Context, uri string) (Crumb, error) {
 
-	ttl := 5
+	ttl := 300
 	key := ""
+
+	u, err := url.Parse(uri)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse URI, %w", err)
+	}
+
+	q := u.Query()
+
+	q_ttl := q.Get("ttl")
+
+	if q_ttl != "" {
+
+		v, err := strconv.Atoi(q_ttl)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse ?ttl= parameter, %w", err)
+		}
+
+		ttl = v
+	}
 
 	crumb_uri, err := NewRandomEncryptedCrumbURI(ctx, ttl, key)
 
